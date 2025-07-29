8bd86c73a326159f7ebda758b1e3315027b1a7e3
KAMALESH J <kamaleshj551@gmail.com>
2025-07-30 00:37:56 +0530

    Update FormController.java

--- Form/src/main/java/com/example/Form/FormController.java
+++ Form/src/main/java/com/example/Form/FormController.java
@@ -9,34 +9,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-//import jakarta.servlet.annotation.MultipartConfig;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000") 
 @RequestMapping("/api")
-//@MultipartConfig(
-//	    maxFileSize = 80485760, // Set to 10 MB in bytes (adjust as needed)
-//	    maxRequestSize = 80971520, // Set to 20 MB in bytes (adjust as needed)
-//	    fileSizeThreshold = 3048576 // 1 MB in bytes
-//	)
+
 public class FormController {
 	@Autowired
     private FormService  formSerives;
 
-//    @PostMapping("/register")
-//    public FormEntity  register(
-//            @ModelAttribute FormEntity formEntity)
-//             throws Exception {
-//        return formSerives.saveRegistration(formEntity);
-//    }
+
 	@PostMapping("/register")
 	public Student register(
 	        @ModelAttribute FormEntity formEntity,
 	        @RequestParam("photo") MultipartFile photoFile) throws Exception {
 
-//	    if (photoFile != null && !photoFile.isEmpty()) {
-//	        formEntity.setPhoto(photoFile.getBytes());
-//	    }
+
 
 	    return formSerives.saveRegistration(formEntity,photoFile.getBytes());
 	}
